2fa4341480d7217bc863d91dadde5d48f4aaa7e1
James Kirk <james.kirk84@gmail.com>
2015-09-08 15:49:48 +0000

    Initial commit

---
+++ src/viewer/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("viewer")>]
+[<assembly: AssemblyProductAttribute("viewer")>]
+[<assembly: AssemblyDescriptionAttribute("A linked data viewer for the knowledge base project")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"

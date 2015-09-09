75c18b9f4131cb5b54b5b9ab8bd25ae456b1a91b
Nathan Smith <nathan@smeef.co.uk>
2015-09-09 14:24:23 +0100

    tidy and remove downcast

--- src/viewer/Server.fs
+++ src/viewer/Server.fs
@@ -5,12 +5,11 @@ open Suave.Http
 open Suave.Types
 open Viewer.App
 
-
 [<EntryPoint>]
 let main argv =
-  //let defaultConfig = { defaultConfig with 
+  //let defaultConfig = { defaultConfig with
   //                                      listenTimeout   = System.TimeSpan.FromMilliseconds 2000.
   //                                      bindings = [ HttpBinding.mk' HTTP "127.0.0.1" 8038 ]
   //                    }
-  startWebServer defaultConfig (app :?> WebPart)
-  1  
+  startWebServer defaultConfig app
+  1

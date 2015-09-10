d1175d1db67575663fe4f55215392ab897848694
James Kirk <james.kirk84@gmail.com>
2015-09-10 14:45:47 +0000

    added test script

--- tests/viewer.Tests/Script.fsx
+++ tests/viewer.Tests/Script.fsx
@@ -1,1 +1,31 @@
-﻿
+﻿#I "bin/Release"
+#r "bin/Release/CsQuery.dll"
+#r "bin/Release/DotLiquid.dll"
+#r "bin/Release/FsPickler.dll"
+#r "bin/Release/Fuchu.dll"
+#r "bin/Release/nunit.framework.dll"
+#r "bin/Release/Suave.dll"
+#r "bin/Release/Suave.DotLiquid.dll"
+#r "bin/Release/Suave.Testing.dll"
+#r "bin/Release/viewer.Tests.dll"
+
+open Suave
+open Suave.Http.Successful
+open Suave.Web
+open Suave.Http
+open Suave.Types
+open Suave.Testing
+open Suave.Http.Applicatives
+open NUnit.Framework
+open CsQuery
+open Viewer.Tests
+
+DotLiquid.setTemplatesDir("bin/Release/templates/")
+
+let getAnnotations =
+  [("Annotation Name", "http://ld.nice.org.uk/ns/Annotation_Uri")]
+
+let checkbox =
+  MakeRequest' HttpMethod.GET "/" getAnnotations
+  |> (fun x -> x.Select("input"))
+

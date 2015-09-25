e1440706c7e4f704caf7664364ee0cf0013d71ec
Nathan Smith <nathan@smeef.co.uk>
2015-09-25 12:10:19 +0100

    flat vocab terms

--- src/viewer/LoadVocabularies.fsx
+++ src/viewer/LoadVocabularies.fsx
@@ -7,7 +7,7 @@
 open FSharp.RDF
 type Term = {
   Uri : Uri
-  Label : string 
+  Label : string
   Parents : Term list
   }
 with static member from x =
@@ -33,17 +33,7 @@ with static member from x =
 ///Load all resources from uri and make a map of rdfs:label -> resource uri
 let vocabLookup uri =
   let gcd = Graph.loadFrom uri
-  printf "%s" ( Graph.print gcd )
   Resource.fromType (Uri.from "http://www.w3.org/2002/07/owl#Class") gcd
   |> List.map Term.from
   |> List.map (Term.walk >> List.rev >> List.map string)
-
-  //  Resource.fromPredicate rdfslbl gcd
-  //|> List.map (fun r ->
-    //   match r with
-      // | FunctionalDataProperty rdfslbl xsd.string x -> printfn "%A" x
-       //| r -> printfn "%A" r)
-
-//"http://www.w3.org/2000/01/rdf-schema#subClassOf"
-// "yhttp://www.w3.org/2000/01/rdf-schema#label"
-
+  |> List.concat |> Seq.distinct |> Seq.toList

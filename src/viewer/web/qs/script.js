63bad62940690e851163e71f761306f8d417c846
Nathan Smith <nathan@smeef.co.uk>
2015-12-14 12:07:33 +0000

    ie fix, js bubbles up and hopefully event stuff works for ie

--- src/viewer/web/qs/script.js
+++ src/viewer/web/qs/script.js
@@ -41,8 +41,51 @@ showLeft.addEventListener('click', function(){
 
 function setupAccordionEvents(){
 
+    addEvent( document.getElementById( 'filters' ), 'click', function(e) {
+	      if (e.target && e.target.className && ~e.target.className.indexOf('accordion-trigger')) {
+            toggleAccordianState.call( e.target, e );
+	      }
+    });
+
+    function toggleAccordianState( e ) {
+        var className = (this.className || '').split(' ');
+        if ( ~className.indexOf( 'open' ) ) {
+            removeClass( this, 'open' );
+            var siblings = getSiblings(this);
+            for(var s = 0; s < siblings.length; s++){
+                addClass( siblings[s], 'closed' );
+            }
+            return;
+        }
+
+        addClass( this, 'open' );
+        var siblings = getSiblings(this);
+        for(var s = 0; s < siblings.length; s++){
+            removeClass( siblings[s], 'closed' );
+        }
+    }
+
+    function addClass( el, cls ) {
+        var className = ( el.className || '' ).split( ' ' );
+        if ( !~className.indexOf( cls ) ) {
+            className.push( cls );
+            el.className = className.join( ' ' );
+        }
+        return el;
+    }
+
+    function removeClass( el, cls ) {
+        var className = ( el.className || '' ).split(' ');
+        if ( ~className.indexOf( cls ) ) {
+            className.splice( className.indexOf( cls ), 1 );
+            el.className = className.join( ' ' );
+        }
+        return el;
+    }
+
     function getChildren(n, skipMe){
         var r = [];
+        n = n.firstChild;
         for ( ; n; n = n.nextSibling )
             if ( n.nodeType == 1 && n != skipMe)
                 r.push( n );
@@ -50,18 +93,18 @@ function setupAccordionEvents(){
     };
 
     function getSiblings(n) {
-        return getChildren(n.parentNode.firstChild, n);
+        return getChildren(n.parentNode, n);
     }
 
-    var accordians = document.getElementsByClassName("accordion-trigger");
-    for (var i = 0; i < accordians.length; i++) {
-        accordians[i].addEventListener('click', function() {
-            this.classList.toggle('open');
-            var siblings = getSiblings(this);
-            for(var s = 0; s < siblings.length; s++){
-                siblings[s].classList.toggle('open');
-            }
-        });
+    function addEvent(elem, evnt, func) {
+        if (elem.addEventListener)  // W3C DOM
+            elem.addEventListener(evnt, func, false);
+        else if (elem.attachEvent) { // IE DOM
+            elem.attachEvent( "on"+ evnt, func );
+        }
+        else { // No much to do
+            elem[ evnt ] = func;
+        }
     }
 
 };

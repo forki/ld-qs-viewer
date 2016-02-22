3ac2ca3583a58901f2747da91e5fbd38734ac22f
Nathan Smith <nathan@smeef.co.uk>
2016-02-22 22:42:12 +0000

    bitta spacing

--- src/viewer/App.fs
+++ src/viewer/App.fs
@@ -14,7 +14,7 @@ let createApp config =
   choose
         [GET >=> path "/qs" >=> request(fun req -> Home.page req config true)
          GET >=> path "/qs/search" >=> request(fun req -> Home.page req config false)
-         GET >=>pathScan "/qualitystandards/%s" (fun (filename) -> request  (fun req -> Resource.page req filename))
+         GET >=> pathScan "/qualitystandards/%s" (fun (filename) -> request  (fun req -> Resource.page req filename))
          GET >=> path "/annotationtool" >=> request(fun req -> AnnotationTool.page req config false)
          GET >=> path "/annotationtool/toyaml" >=> request(fun req -> AnnotationTool.page req config true)
          GET >=> browseHome

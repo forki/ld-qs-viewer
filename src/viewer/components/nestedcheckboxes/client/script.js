148e5beb168b50af1194447cdaae6e516329fe26
Nathan Smith <nathan@smeef.co.uk>
2016-04-05 16:13:29 +0100

    retain selection state

--- src/viewer/components/nestedcheckboxes/client/script.js
+++ src/viewer/components/nestedcheckboxes/client/script.js
@@ -2,47 +2,12 @@
 $(function() {
     // Apparently click is better chan change? Cuz IE?
     $('.filter-checkbox').change(function(e) {
-        var checked = $(this).prop("checked"),
-            container = $(this).parent();
-
-        container.find('input[type="checkbox"]').prop({
-            indeterminate: false,
-            checked: checked
-        });
-
-        function checkSiblings(el) {
-            var parent = el.parent().parent(),
-                all = true;
-
-            el.siblings().each(function() {
-                return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
-            });
-
-            if (all && checked) {
-                parent.children('input[type="checkbox"]').prop({
-                    indeterminate: false,
-                    checked: checked
-                });
-                checkSiblings(parent);
-            } else if (all && !checked) {
-                parent.children('input[type="checkbox"]').prop("checked", checked);
-                parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
-                checkSiblings(parent);
-            } else {
-                el.parents("li").children('input[type="checkbox"]').prop({
-                    indeterminate: true,
-                    checked: false
-                });
-            }
-        }
-
-        checkSiblings(container);
+        determinateCheckboxTree(this);
     });
 
-
-    $('.filter-checkbox:checked').each(function(){
-        var checked = $(this).prop("checked"),
-            container = $(this).parent();
+    function determinateCheckboxTree (context){
+        var checked = $(context).prop("checked"),
+            container = $(context).parent();
 
         container.find('input[type="checkbox"]').prop({
             indeterminate: false,
@@ -76,6 +41,11 @@ $(function() {
         }
 
         checkSiblings(container);
+
+    }
+
+    $('.filter-checkbox:checked').each(function(){
+        determinateCheckboxTree(this);
     });
 
 });

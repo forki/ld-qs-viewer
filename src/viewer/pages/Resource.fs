037651d617b135a88cc069ed60920377d0a27219
Nathan Smith <nathan@smeef.co.uk>
2016-04-06 13:19:33 +0100

    now including hotjar

--- src/viewer/pages/Resource.fs
+++ src/viewer/pages/Resource.fs
@@ -1,19 +1,28 @@
 module Viewer.Pages.Resource
 
 open Suave
-open Suave.Cookie
 open System.IO
+open Suave.Cookie
+open Viewer.AppConfig
+open Viewer.Components
+open Viewer.Components.Hotjar
+
 
 type ResourceModel = {
   Content : string
+  Scripts : string
 }
+
+let private buildScripts config =
+    sprintf """ <script src="/qs/components/googleanalytics/client/script.js"></script>
+    %s """ (Hotjar.render config.HotjarId)
 
 let private artifacts testing filename =
   match testing with
     | true -> sprintf "/test_artifacts/published/qualitystandards/%s" filename
     | false -> sprintf "/artifacts/published/qualitystandards/%s" filename
 
-let page (request:HttpRequest) filename =
+let page (request:HttpRequest) config filename =
   let testing = request.cookies |> Map.containsKey "test"
   let content =
     try
@@ -21,5 +30,5 @@ let page (request:HttpRequest) filename =
     with
       | ex -> "Could not find resource."
 
-  DotLiquid.page "templates/resource.html" {Content = content}
+  DotLiquid.page "templates/resource.html" { Content = content; Scripts = buildScripts config }
 

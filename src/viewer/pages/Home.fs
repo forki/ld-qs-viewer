2978ba2a9a3b3d332930d891931057d2c163ebb7
David Wong <wongcheming@gmail.com>
2016-05-24 13:46:49 +0100

    Added the nice tracking script and a test track event

--- src/viewer/pages/Home.fs
+++ src/viewer/pages/Home.fs
@@ -32,7 +32,11 @@ let private buildScripts =
      <script src="/qs/components/googleanalytics/client/script.js"></script>
      <script src="/qs/components/jquery/client/script.js"></script>
      <script src="/qs/components/nestedcheckboxes/client/script.js"></script>
-"""
+     <script src="//cdn.nice.org.uk/V3/Scripts/nice/NICE.EventTracking.js" async></script>
+     <script defer>
+      if ($.fn.trackevent) $('.counter').trackevent('Search Results', { action: 'Zero Search Results', label: 'penguin gutterfff' });
+    </script>
+  """
 
 let page (req:HttpRequest) config showOverview =
 

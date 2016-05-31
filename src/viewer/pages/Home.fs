2c4b14283151e086565f28fe3c02c23572a72e69
David Wong <wongcheming@gmail.com>
2016-05-31 10:36:51 +0100

    debuging error on test

--- src/viewer/pages/Home.fs
+++ src/viewer/pages/Home.fs
@@ -40,6 +40,7 @@ let private buildScripts =
      <script src="/qs/components/jquery/client/script.js?version=1"></script>
      <script src="/qs/components/nestedcheckboxes/client/script.js?version=1"></script>
      <script src="/qs/components/main/client/script.js?version=1"></script>
+     <script src="/qs/components/main/client/jquery.scrolldepth.min.js?version=1"></script>
   """
 
 let page (req:HttpRequest) config showOverview =

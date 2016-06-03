72590fafc1fc7225d0c5e269fdeee84cfc7c0a63
David Wong <wongcheming@gmail.com>
2016-06-03 11:51:42 +0100

    added test for the links

--- tests/viewer.FrontendTests/test/main.js
+++ tests/viewer.FrontendTests/test/main.js
@@ -7,7 +7,14 @@ describe("Given there has been a filter selected", function(){
 });
 
 describe("Given the clear all button has been clicked", function(){
-  it("should send a clear event to GA", function() {
-    jQuery.withArgs("a[type='reset']").calledOnce.should.be.true;
-  });
+    it("should send a clear event to GA", function() {
+        jQuery.withArgs("a[type='reset']").calledOnce.should.be.true;
+    });
+});
+
+describe("Links in the site", function(){
+    it("should send an event to GA", function() {
+        jQuery.withArgs("#mailFeedback").calledOnce.should.be.true;
+        jQuery.withArgs("#old-standards").calledOnce.should.be.true;
+    });
 });

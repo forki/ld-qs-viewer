6d50344e4ef5643c550908aff9bacef7d9e90648
James Kirk <james.kirk84@gmail.com>
2016-09-15 09:15:33 +0000

    adding ontologies holding page

--- src/viewer/App.fs
+++ src/viewer/App.fs
@@ -16,6 +16,7 @@ let createApp config =
          GET >=> path "/qs/search" >=> request(fun req -> Home.page req config false)
          GET >=> pathScan "/resource/%s" (fun resourceId -> Resource.page config resourceId)
          GET >=> pathScan "/things/%s" (fun resourceId -> Resource.page config resourceId)
+         GET >=> path "/ontologies" >=> (Successful.OK "Welcome to ontologies")
          GET >=> buildPath "/annotationtool" >=> request(fun req -> AnnotationTool.page req config false)
          GET >=> path "/annotationtool/toyaml" >=> request(fun req -> AnnotationTool.page req config true)
          GET >=> browseHome

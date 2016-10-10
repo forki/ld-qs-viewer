c70fec6c3692f44be4b6db2d1f18e28b0a36fe0b
David Wong <wongcheming@gmail.com>
2016-10-10 14:55:31 +0100

    Run one test category

--- build.fsx
+++ build.fsx
@@ -157,6 +157,7 @@ Target "RunTests" (fun _ ->
     !! testAssemblies
     |> NUnit (fun p ->
         { p with
+            IncludeCategory = "RunOnly"
             DisableShadowCopy = true
             TimeOut = TimeSpan.FromMinutes 20.
             OutputFile = "TestResults.xml" })

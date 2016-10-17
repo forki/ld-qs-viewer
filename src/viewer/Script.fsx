7727fd820c2d3d403baa6233387473efb304ae43
David Wong <wongcheming@gmail.com>
2016-10-17 12:03:42 +0100

    Saving my test file for keeps sakes

--- src/viewer/Script.fsx
+++ src/viewer/Script.fsx
@@ -10,6 +10,7 @@
 type TermD = 
   {
   Label : string
+  Property : string
   Children : Term list
   } 
 and Term =
@@ -18,25 +19,29 @@ and Term =
 
 let Term1 = Term {
             Label = "d"
+            Property = "Property D"
             Children= []}
 
 let terms = [Term {
               Label = "a"
+              Property = "Property A"
               Children = [
                             Term {
                               Label = "b"
+                              Property = "Property B"
                               Children = [
-                                         Term { Label = "c"; Children=[]} 
-                                         Term { Label = "c"; Children=[]} 
-                                         Term { Label = "c"; Children=[]} 
+                                         Term { Label = "c"; Property = "C"; Children=[]} 
+                                         Term { Label = "c"; Property = "C"; Children=[]} 
+                                         Term { Label = "c"; Property = "C"; Children=[]} 
                               ]
                             };
                             Term {
                               Label = "bb"
+                              Property = "BB"; 
                               Children = [
-                                         Term { Label = "cc"; Children=[]} 
-                                         Term { Label = "cc"; Children=[]} 
-                                         Term { Label = "cc"; Children=[]} 
+                                         Term { Label = "cc"; Property = "CC"; Children=[]} 
+                                         Term { Label = "cc"; Property = "CC"; Children=[]} 
+                                         Term { Label = "cc"; Property = "CC"; Children=[]} 
                               ]
                             };
                             
@@ -44,25 +49,48 @@ let terms = [Term {
             };
             Term1;]
 
-
-let rec loopChildren start children =
-  let test x filters=
-    Seq.exists (fun a -> a=x) filters
-
-  List.fold (fun acc x -> match x with 
-                          | Term x -> if (test x.Label ["a";"c"]) then
-                                         acc
-                                      else
-                                         acc @ loopChildren [x.Label] x.Children
-                          | Empty -> []) start children
+(* return each item specified in the filter *)
     
-loopChildren [] terms
-(* let rec loopChildren start children =*)
+let getItems searchByProperty getProperty searchTerms =
+  let searchFn filters searchByProperty x =
+    Seq.exists (fun a ->a = searchByProperty x) filters
+
+  let search currentTerm =
+   searchFn searchTerms searchByProperty currentTerm
+  
+  let rec recurseTree start children =
+    List.fold (fun acc term ->  match term with
+                                | Term term -> if (search term) then
+                                                  [getProperty term] @ acc @ recurseTree start term.Children
+                                                else
+                                                  acc @ recurseTree start term.Children
+                                | Empty -> []) start children
+
+  recurseTree [] terms
+
+let propertyToSearchFor term = term.Property 
+let propertyToRetrieve term = term.Property 
+
+getItems propertyToSearchFor propertyToRetrieve ["a";"c"]
+
+(* basic recursion *)
+
+(* let rec loopChildren start children  f =*)
+(*   let test x filters=*)
+(*     Seq.exists (fun a->a=x) filters*)
+
 (*   List.fold (fun acc x -> match x with *)
+(*                           | Term x -> if (f x) then*)
+(*                                         [x.Label] @ acc @ loopChildren start x.Children f*)
+(*                                       else*)
+(*                                         acc @ loopChildren start x.Children f*)
+(*                           | Empty -> []) start children*)
+
+(* Return each element as it is *)
+(* let rec loopChildren start children =*)
+(*   List.fold (fun acc x -> match x with*)
 (*                           | Term x ->  acc @ loopChildren [x.Label] x.Children*)
 (*                           | Empty -> []) start children*)
-    
-(* loopChildren [] terms*)
 
 
 let a = [1;2;3;4;]

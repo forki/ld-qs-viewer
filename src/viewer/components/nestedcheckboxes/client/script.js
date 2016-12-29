fa4e756587a7ce1417b4bae5bf3abe60f4e1c0df
Stewart Ainsworth <stewart.ainsworth@nice.org.uk>
2016-12-29 14:03:54 +0000

    nestedcheckboxes script re-worked to show up and down inference - but only have selected terms in query

--- src/viewer/components/nestedcheckboxes/client/script.js
+++ src/viewer/components/nestedcheckboxes/client/script.js
@@ -1,51 +1,49 @@
-//NESTED CHECKBOXES - https://css-tricks.com/indeterminate-checkboxes/
 $(function() {
-    // Apparently click is better chan change? Cuz IE?
     $('.filter-checkbox').change(function(e) {
-        determinateCheckboxTree(this);
+        ShowInferenceInTree(this);
     });
 
-    function determinateCheckboxTree (context){
-        var checked = $(context).prop("checked"),
-            container = $(context).parent();
-
-        container.find('input[type="checkbox"]').prop({
-            indeterminate: false,
-            checked: checked
-        });
-
-        function checkSiblings(el) {
-            var parent = el.parent().parent(),
-                all = true;
-
-            el.siblings().each(function() {
-                return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
-            });
-
-            if (all && checked) {
-                parent.children('input[type="checkbox"]').prop({
-                    indeterminate: false,
-                    checked: checked
-                });
-                checkSiblings(parent);
-            } else if (all && !checked) {
-                parent.children('input[type="checkbox"]').prop("checked", checked);
-                parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
-                checkSiblings(parent);
-            } else {
-                el.parents("li").children('input[type="checkbox"]').prop({
-                    indeterminate: true,
-                    checked: false
-                });
-            }
-        }
-
-        checkSiblings(container);
-
+    function anyChildrenSelected (container) {
+      return ($(container).find('input[type="checkbox"]').filter(function (ix, itm) {
+        return $(itm).prop("checked");
+      }).length > 0);
     }
 
-    $('.filter-checkbox:checked').each(function(){
-        determinateCheckboxTree(this);
-    });
+    function highlight(container, upTree) {
+      let children = container.find('input[type="checkbox"]');
+      
+      children.each(function(index, item){
+        let thisChecked = $(item).prop("checked");
+        let highlight = true;
+
+        if (upTree) {
+          let thisContainer = $(item).parent();
+          highlight = anyChildrenSelected(thisContainer);
+        }
+
+        if (thisChecked) {
+          $(item).prop( {indeterminate: false});
+        } else {
+          $(item).prop( {indeterminate: highlight});
+        }
+      });
+    }
+
+    function ShowInferenceInTree (context) {
+      var container = $(context).parent();
+      
+      while (container.parent().parent("li").length > 0){
+        container = container.parent().parent("li");
+      }
+      
+      highlight(container, true);
+      
+      $(container).find('input[type="checkbox"]').filter(function (ix, itm) {
+        return $(itm).prop("checked");
+      }).each(function(index, item){
+        highlight($(item).parent(), false);
+      })
+
+    }
 
 });
